d8d0b62881bd866ba3f3080a1a706dc60cae956d
dillby777 <dillby@gmamil.com>
2025-04-16 21:17:06 -0400

    renamed:    custom_components/xbox_game_cover_art/__initi__.py -> custom_components/xbox_game_cover_art/__init__.py 	modified:   custom_components/xbox_game_cover_art/manifest.json

--- custom_components/xbox_game_cover_art/__init__.py
+++ custom_components/xbox_game_cover_art/__init__.py
@@ -13,7 +13,6 @@ async def async_setup_entry(hass, config_entry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
 
-    # 👇 This is what triggers sensor.py
     await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor"])
 
     return True
